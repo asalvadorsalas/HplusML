4634a9d1aced317f828b24a7a5a62a688263f4a9
jglatzer <jglatzer@cern.ch>
2019-03-04 14:57:25 +0100

    added signal only scaler class

--- HpMLUtils.py
+++ HpMLUtils.py
@@ -353,6 +353,50 @@ class WeightedStandardScaler(BaseEstimator, TransformerMixin):
             X *= self.scale_
         return X
 
+class MultiSWeightsScaler():
+    """ Class that scales makes the integral of the signal weights be 1., for several signal categories the distribution as a function of the class variable is flattened. Background is not considered."""
+
+    def _reset(self):
+        """Reset internal data-dependent state of the scaler, if necessary.
+        __init__ parameters are not touched.
+        """
+
+        # Checking one attribute is enough, becase they are all set together
+        # in partial_fit
+        if hasattr(self, 'scale_'):
+            del self.scale_
+
+    def fit(self,X,y, sample_weight):
+        """learns the sum of weights for all classes and calculates a scale factor for each class so that the sum of weights for the signal is flattened as a function of the class variable (and the integral is 1.0)
+           X: feature matrix, ignored
+           y: series of class labels
+           sample_weight: Series of sample weights
+        """
+        classes=sorted(y.unique())
+        differences={}
+        #set the differences between signal points
+        differences={classes[i]:(classes[i+1]-classes[i-1])/2 for i in range(1,len(classes)-1) if classes[i]>0}
+        differences[classes[0]]=classes[1]-classes[0]
+        differences[classes[-1]]=classes[-1]-classes[-2]
+        diffsum=sum(differences.values())
+        #print differences, "->", diffsum
+        self.scale_={}
+        for classlabel in classes:
+            sumweight=sample_weight[y==classlabel].sum()
+            self.scale_[classlabel]=differences[classlabel]/(sumweight*diffsum)
+         return
+        
+    def transform(self, X, y, sample_weight, copy=None):
+        """Transforms the sum of weights for all classes so that sum of weights for the signal is flattened as a function of the class variable (and the integral is 1.0)
+           X: feature matrix, ignored
+           y: series of class labels
+           sample_weight: Series of sample weights
+        """
+        
+        for classlabel in self.scale_:
+            sample_weight[y==classlabel]*=self.scale_[classlabel]
+        return X
+
 class MultiSBWeightsScaler():
     """ Class that scales makes the integral of the signal/background weights be 0.5, for several signal categories the distribution as a function of the class variable is flattened"""
 
