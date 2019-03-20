f4ccd47c80fc675d05a324527840375e9936120e
jglatzer <jglatzer@cern.ch>
2019-03-20 11:58:23 +0100

    adding keras helper utils

--- HpKerasUtils.py
+++ HpKerasUtils.py
@@ -5,6 +5,7 @@ from keras.layers import Dense, Dropout
 from keras import regularizers
 from sklearn.metrics import roc_auc_score
 from keras.callbacks import Callback, EarlyStopping, ModelCheckpoint
+import matplotlib.pyplot as plt
 
 class RocCallback(Callback):
     """ A Keras callback that calculates the ROC AUC"""
